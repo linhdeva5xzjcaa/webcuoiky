ef199e93b7fc38805c02e0c7f8f68aba53c01ebb
PhvnDeng <dengphvn206@gmail.com>
2026-04-11 13:44:00 +0700

    Update book.js

--- book.js
+++ book.js
@@ -1,13 +1,11 @@
-  let slides = document.querySelectorAll(".slideshow img");
+let slides = document.querySelectorAll(".slideshow .slide");
   let index = 0;
 
   function showSlide() {
     slides.forEach((slide, i) => {
       slide.classList.remove("active");
-      if (i === index) {
-        slide.classList.add("active");
-      }
     });
+    slides[index].classList.add("active");
     index = (index + 1) % slides.length;
   }
 
