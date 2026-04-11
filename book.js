bb9536ef3f33c4a1930457e1a21a30a40c49e74f
PhvnDeng <dengphvn206@gmail.com>
2026-04-11 13:50:30 +0700

    Update book.js

--- book.js
+++ book.js
@@ -1,12 +1,12 @@
-let slides = document.querySelectorAll(".slideshow .slide");
+document.addEventListener("DOMContentLoaded", function() {
+  let slides = document.querySelectorAll(".slideshow .slide");
   let index = 0;
 
   function showSlide() {
-    slides.forEach((slide, i) => {
-      slide.classList.remove("active");
-    });
+    slides.forEach(slide => slide.classList.remove("active"));
     slides[index].classList.add("active");
     index = (index + 1) % slides.length;
   }
 
   setInterval(showSlide, 3000); // đổi ảnh sau mỗi 3 giây
+});
